f85fbf01055cf334bde4b7e6d9562a1be64da49a
Lucas Teske <lucas@teske.com.br>
2014-04-03 14:54:10 -0300

    Added GPL License header. Re-idented the code.

--- android/src/br/energylabs/forum/verWeb.java
+++ android/src/br/energylabs/forum/verWeb.java
@@ -1,40 +1,58 @@
+/*
+ *  EnergyLabs DangerCenter Forum Android App
+    Copyright (C) 2014  DangerCenter
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  
+  	Made by: 
+  		Victor Silva ( viitorsant at gmail dot com )
+  		Lucas Teske ( lucas at teske dot net dot br )	
+ */
 package br.energylabs.forum;
 
-import android.content.*;
-import android.graphics.*;
 import android.os.*;
 import android.widget.*;
 import android.app.*;
-import android.net.*;
 import android.webkit.*;
 import android.view.*;
 
-public class verWeb extends Activity{
-	
+public class verWeb extends Activity {
+
 	// Variaveis dos widgets.
 	WebView web;
 	ProgressBar loading;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        web = (WebView) findViewById(R.id.webview01);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		web = (WebView) findViewById(R.id.webview01);
 		loading = (ProgressBar) findViewById(R.id.load);
-        
+
 		// Abrir o WebView com o link do forum.
-        web.setWebViewClient(new w2());
-        web.getSettings().setJavaScriptEnabled(true);
+		web.setWebViewClient(new w2());
+		web.getSettings().setJavaScriptEnabled(true);
 		web.getSettings().setLoadsImagesAutomatically(true);
-        web.loadUrl("http://forum.energylabs.com.br");
+		web.loadUrl("http://forum.energylabs.com.br");
 
 	}
-	
-    // Voltar ao clicar no "Retornar".
+
+	// Voltar ao clicar no "Retornar".
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) 
-	{
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
 			web.goBack();
 			return true;
@@ -42,20 +60,14 @@ public class verWeb extends Activity{
 		return super.onKeyDown(keyCode, event);
 	}
 
- private class w2 extends WebViewClient
-{
-	@Override
-	public void onPageStarted(WebView view, String url) {
-		
-		super.onPageStarted(view, url, null);
-	}
+	private class w2 extends WebViewClient {
 
-	@Override
-	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		
-		view.loadUrl(url);
-		return true;
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
 
+			view.loadUrl(url);
+			return true;
+
+		}
 	}
-  }
 }
